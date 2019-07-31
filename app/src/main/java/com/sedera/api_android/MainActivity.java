26ac064739aa9b2260204e136ad0713e695d377a
nomentsoa sedera <starksedera@gmail.com>
2019-07-30 19:25:45 -0800

    first-page
    add two text view and button

--- app/src/main/java/com/sedera/api_android/MainActivity.java
+++ app/src/main/java/com/sedera/api_android/MainActivity.java
@@ -2,12 +2,29 @@ package com.sedera.api_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textView)
+    TextView title;
+
+    @BindView(R.id.textView2)
+    TextView content;
+
+    @BindView(R.id.button)
+    Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
